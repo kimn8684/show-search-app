0787283f48b696f8e63e3719e33522f881a6b439
Nuri Kim <kimn8684@gmail.com>
2019-03-03 14:59:22 -0800

    basic structure of the show search project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShowDetailsComponent } from './show-details/show-details.component';
+import { ShowSearchComponent } from './show-search/show-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowDetailsComponent,
+    ShowSearchComponent
   ],
   imports: [
     BrowserModule,
