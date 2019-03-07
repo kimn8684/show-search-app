f635173fe1dc128407ea78fee9b549fa224faaa0
Nuri Kim <kimn8684@gmail.com>
2019-03-07 01:12:10 -0800

    adjust spacing

--- src/app/ishow-details.ts
+++ src/app/ishow-details.ts
@@ -6,5 +6,4 @@ export interface IShowDetails {
     schedule: string
     image: string
     summary: string
-
 }
--- src/app/show-service.service.ts
+++ src/app/show-service.service.ts
@@ -1,22 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { IShowDetails } from './ishow-details';
+import { map } from 'rxjs/operators'
 
-import {Injectable} from '@angular/core';
-import{HttpClient} from '@angular/common/http';
-import{environment} from 'src/environments/environment';
-import {IShowDetails} from './ishow-details';
-import{map} from 'rxjs/operators'
-
-interface IShowDetailsData{
+interface IShowDetailsData {
+  name: string,
   schedule: {
     time: string,
-    days: string []
-   },
-   name: string,
-   genres: string[],
-   network: {name: string},
-   image: {original: string},
-   summary: string
-  
-  }
+    days: string[]
+  },
+  genres: string[],
+  network: {name: string},
+  image: {original: string},
+  summary: string
+}
 
 
 @Injectable({
@@ -29,12 +27,10 @@ export class ShowService {
 
   getCurrentShow(name: string){
     return this.httpClient.get<IShowDetailsData>(`${environment.baseUrl}api.tvmaze.com/singlesearch/shows/?q=${name}`).pipe(
-      map(data => this.transformToIShowDetails(data))
-    )
-
+      map(data => this.transformToIShowDetails(data)))
   }
 
-  private transformToIShowDetails(data:IShowDetailsData) : IShowDetails{
+  private transformToIShowDetails(data: IShowDetailsData): IShowDetails {
     return {
       time: data.schedule.time,
       schedule: data.schedule.days[''],
@@ -43,7 +39,6 @@ export class ShowService {
       network: data.network.name,
       image: data.image.original,
       summary: data.summary
-      
-    }
-  }
+    };
+  };
 }
