7a0a7f82339cd7bc6b1f277d5be65018c2319f76
Mel Wright <codingwright@gmail.com>
2019-03-08 23:48:45 -0800

    MW copying over, test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { ShowDetailsComponent } from './show-details/show-details.component';
 import { ShowSearchComponent } from './show-search/show-search.component';
 import { ShowService } from './show.service';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule, MatFormFieldModule, MatInputModule } from '@angular/material';
 
 
 @NgModule({
@@ -18,7 +21,26 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [ShowService],
   bootstrap: [AppComponent]
--- src/app/show-search/show-search.component.ts
+++ src/app/show-search/show-search.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-show-search',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowSearchComponent implements OnInit {
 
+  search = new FormControl("", [Validators.minLength(2)]);
+
+  @Output() searchEvent = new EventEmitter<string>()
+
   constructor() { }
 
   ngOnInit() {
+    this.search.valueChanges.pipe(debounceTime(1000)).subscribe((searchValue: string) => {
+      if (!this.search.invalid) {
+        this.searchEvent.emit(searchValue)
+      }
+    });
   }
 
 }
